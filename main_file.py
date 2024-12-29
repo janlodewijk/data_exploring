662287cec15bd3731794f60b5177f8f37ccea050
janlodewijk <160859694+janlodewijk@users.noreply.github.com>
2024-12-29 21:45:14 +0100

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 url = 'https://api.data.amsterdam.nl/dcatd/datasets/yvlbMxqPKn1ULw/purls/1/subsidies_openbaar_subsidieregister.csv'
 response = requests.get(url)
-response.raise_for_status()  # Controleer of de aanvraag succesvol was
+response.raise_for_status()
 
-# Gebruik een buffer om de inhoud direct in pandas te lezen
 from io import StringIO
 csv_data = StringIO(response.text)
 df = pd.read_csv(csv_data)
